b955f5648bb5d170a92f2a44808d7b50e422b5cc
matthew <iwasnothing@gmail.com>
2021-01-03 20:40:58 +0800

    py module

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ["matplotlib","pandas","yfinance","lxml","alpaca-trade-api","ray","pytorch-lighning"]
+
+setup(
+  name='stock',
+  version='1.0',
+  install_requires=REQUIRED_PACKAGES,
+  packages=find_packages(),
+  include_package_data=True,
+  description='AI Platform Boston samples'
+)
